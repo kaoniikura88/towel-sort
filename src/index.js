4ce6cc111eb50bf7fbde2b3ea59e662a30994b61
kaoniikura88 <sapozhnikova_anastasia@mail.ru>
2021-03-07 16:36:36 +0300

    feat: sort array

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (!matrix) {
+        return [];
+    }
+    let sorted = []
+    for (let i=0; i<matrix.length; i++) {
+        if(i%2 ===0) {
+            sorted = sorted.concat(matrix[i])
+        } else sorted = sorted.concat((matrix[i]).reverse())
+    }
+    return sorted;
 }
